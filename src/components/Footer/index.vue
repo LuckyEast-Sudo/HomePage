c85ab73649f657022968c62b3564fea4eafe676d
东东 <47738269+LuckyEast-Sudo@users.noreply.github.com>
2023-06-07 10:51:34 +0800

    1.3

--- src/components/Footer/index.vue
+++ src/components/Footer/index.vue
@@ -3,7 +3,7 @@
     <div class="power" v-show="!store.playerState">
       <span
         >Copyright&nbsp;&copy;&nbsp;{{ fullYear }}
-        <a href="https://luckyeast.eu.org/">东东</a>
+        <a href="https://luckyworld.eu.org/">东东</a>
       </span>
       <!-- 以下信息请不要修改哦 -->
       <span class="hidden"
