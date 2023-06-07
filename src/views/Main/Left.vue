7538374196ab951d2a6dcc13c69a7463410eeb4e
东东 <47738269+LuckyEast-Sudo@users.noreply.github.com>
2023-06-07 14:09:42 +0800

    1.4.1

--- src/views/Main/Left.vue
+++ src/views/Main/Left.vue
@@ -1,14 +1,14 @@
 <template>
   <div :class="store.mobileOpenState ? 'left hidden' : 'left'">
     <Message />
-    <SocialLinks />
+  <!-- SocialLinks / -->
   </div>
 </template>
 
 <script setup>
 import { mainStore } from "@/store";
 import Message from "@/components/Message/index.vue";
-import SocialLinks from "@/components/SocialLinks/index.vue";
+// import SocialLinks from "@/components/SocialLinks/index.vue";
 const store = mainStore();
 </script>
 
